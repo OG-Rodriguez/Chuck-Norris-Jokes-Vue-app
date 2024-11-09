85b322f101887021d2dc7608a08d93f678466483
OG-Rodriguez <rodriguez_gil@yahoo.com>
2024-11-09 08:17:10 -0600

    Task10

--- src/App.vue
+++ src/App.vue
@@ -24,11 +24,11 @@ export default {
   data() {
     return {
       chuck: [
-        { icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png", value: "Chuck Norris can skydive into outer space." },
-        { icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png", value: "The chief export of Chuck Norris is pain." },
-        { icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png", value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
-        { icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png", value: "Time waits for no man. Unless that man is Chuck Norris." },
-        { icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png", value: "If you spell Chuck Norris in Scrabble, you win. Forever." }
+        { icon_url: "https://th.bing.com/th/id/OIP.WWkrj8aJJ_p9PnGRI7w_wQHaHa?w=188&h=188&c=7&r=0&o=5&dpr=1.3&pid=1.7/img/avatar/chuck-norris.png", value: "Chuck Norris can skydive into outer space." },
+        { icon_url: "https://th.bing.com/th/id/OIP.kOjJ9mpNNWlo7jBFZo_RMwHaHa?w=170&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7/img/avatar/chuck-norris.png", value: "The chief export of Chuck Norris is pain." },
+        { icon_url: "https://th.bing.com/th/id/OIP.Qb_qNqQRoftrRVl14FRhDQHaEF?w=301&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7/img/avatar/chuck-norris.png", value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
+        { icon_url: "https://th.bing.com/th/id/OIP.FjrdEi6Jb9N4XpScGNmhCAAAAA?w=132&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7/img/avatar/chuck-norris.png", value: "Time waits for no man. Unless that man is Chuck Norris." },
+        { icon_url: "https://th.bing.com/th/id/OIP.-UVuyBuAhW5g5uIsPnQqSQHaK8?w=115&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7/img/avatar/chuck-norris.png", value: "If you spell Chuck Norris in Scrabble, you win. Forever." }
       ]
     };
   }
